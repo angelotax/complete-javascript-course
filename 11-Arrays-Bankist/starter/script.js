f9c70b39a3295f25ba8de9c162c8a0e10ad9dc37
Angelo Tassie <angelotax@gmail.com>
2023-06-28 15:58:57 +0200

    more methods training

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -70,25 +70,67 @@ const currencies = new Map([
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
-
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+console.log(`----------MAP METHOD----------`);
 
+// map method returns a new array
+// const eurToUsd = 1.1;
+// const movementsTousd = movements.map (function (value) {
+//    return value * eurToUsd;
+// });
+// console.log(movements);
+// console.log(movementsTousd);
+// SAME BUT USING ARROW FUNCTION
+// const eurToUsd = 1.1;
+// const movementsTousd = movements.map(value => value * eurToUsd);
+// console.log(movements);
+// console.log(movementsTousd);
+const diplayMovements = movements.map(
+  (value, index, array) =>
+    `Movement ${index + 1} you ${value > 0 ? 'deposited' : 'witdrew'}${Math.abs(
+      value
+    )}`
+);
+console.log(diplayMovements);
+
+// const value = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// for each method does not return a new array
+// console.log(`------FOR EACH METHOD------`);
+// value.forEach(function (value, index, array) {
+//   if (value > 0) {
+//     console.log(`Movement ${index + 1} you deposited ${value}`);
+//   } else {
+//     console.log(`Movement ${index + 1} you witdrawed ${Math.abs(value)}`);
+//   }
+//   console.log(array);
+// });
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-let arr2 = ['a', 'b', 'c', 'd', 'e'];
-// slice returns new Array leaving original untouched, splice returns new array altering existing array
-console.log(arr);
-console.log('array before splice');
-console.log(arr2);
-console.log('array before slice');
-console.log('---------------');
-console.log(arr.splice(2, 4));
-console.log('new array returned by splice');
-console.log(arr2.slice(2, 4));
-console.log('new array returned by slice');
-console.log('---------------');
-console.log(arr);
-console.log('original array after splice is modified');
-console.log(arr2);
-console.log('original array after slice is the same array');
+// SLice and Splice methods
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr2 = ['a', 'b', 'c', 'd', 'e'];
+// // slice returns new Array leaving original untouched, splice returns new array altering existing array
+// // console.log(arr);
+// // console.log('array before splice');
+// // console.log(arr2);
+// // console.log('array before slice');
+// // console.log('---------------');
+// // console.log(arr.splice(2, 4));
+// // console.log('new array returned by splice');
+// // console.log(arr2.slice(2, 4));
+// // console.log('new array returned by slice');
+// // console.log('---------------');
+// // console.log(arr);
+// // console.log('original array after splice is modified');
+// // console.log(arr2);
+// // console.log('original array after slice is the same array');
+// ----------------------------------------------------------------
+// for loop
+
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(` you deposited ${movement}`);
+//   } else {
+//     console.log(` you witdrawed ${Math.abs(movement)}`);
+//   }
+// }
